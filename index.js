76c41e4e001f867c544cab3a919889623ab05a7c
Sylvia <sylviachebet03@gmail.com>
2024-03-14 20:57:43 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.getElementById("main")
+document.getElementById('main').remove()
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+const yourName = "Sylvia Chebet";
+newHeader.textContent = `${yourName} is the champion`;
+document.body.appendChild(newHeader);
